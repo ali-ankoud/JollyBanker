f1fb2ba1404bebb3421f6fe5c24bca9df6e30254
Ali Ankoud <aankoud@uw.edu>
2022-08-13 15:12:11 -0700

    finalized BSTree.h

---
+++ BSTree.h
@@ -0,0 +1,32 @@
+// by Ali Ankoud and Kimberly Rodriguez
+// 17 August 2022
+// BSTree.h
+
+#pragma once
+#include "Account.h"
+
+class BSTree
+{
+    public:
+        BSTree();
+        ~BSTree();
+
+        bool Insert(Account *);
+        // retrieve object, first parameter is the ID of the account
+        // second parameter holds pointer to found object, NULL if not found
+        bool Retrieve(const int &, Account * &) const;
+        // displays the contents of a tree to cout
+        void Display() const;
+
+        void Empty();
+        bool isEmpty() const;
+    private:
+        struct Node
+        {
+            Account *pAcct;
+            Node *right;
+            Node *left;
+        }; 
+
+        Node *root;
+};
